c9303b0b6ca3dc60622dda6cad0bdd8df96cc2d3
mxro <mxro@nowhere.com>
2014-08-13 12:39:21 +1000

    Writing tests for SQL implementation of async map and fixing issues Autocommit Wed Aug 13 12:39:21 EST 2014

--- src/main/java/de/mxro/async/map/sql/SqlAsyncMapDependencies.java
+++ src/main/java/de/mxro/async/map/sql/SqlAsyncMapDependencies.java
@@ -1,16 +1,23 @@
 package de.mxro.async.map.sql;
 
-
-
-
+import de.mxro.async.map.AsyncMap;
 import de.mxro.factories.Dependencies;
 import de.mxro.serialization.Serializer;
 import de.mxro.serialization.jre.StreamDestination;
 import de.mxro.serialization.jre.StreamSource;
 
-public interface SqlAsyncMapDependencies  extends Dependencies {
+/**
+ * Run-time dependencies for SQL backed {@link AsyncMap}
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
+public interface SqlAsyncMapDependencies extends Dependencies {
 
+	/**
+	 * 
+	 * @return Serializer used to serialize objects submitted to the map.
+	 */
 	public abstract Serializer<StreamSource, StreamDestination> getSerializer();
 
-	
 }
