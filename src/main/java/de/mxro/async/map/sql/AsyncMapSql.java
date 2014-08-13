e58980474da60fbbee72ac7975524214ef6a9b82
mxro <mxro@nowhere.com>
2014-08-13 11:20:46 +1000

    Writing tests for SQL implementation of async map and fixing issues Autocommit Wed Aug 13 11:20:46 EST 2014

--- src/main/java/de/mxro/async/map/sql/AsyncMapSql.java
+++ src/main/java/de/mxro/async/map/sql/AsyncMapSql.java
@@ -1,5 +1,9 @@
 package de.mxro.async.map.sql;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import de.mxro.async.map.AsyncMap;
 import de.mxro.async.map.sql.internal.SqlAsyncMapImplementation;
 import de.mxro.async.map.sql.internal.SqlConnectionFactory;
@@ -22,6 +26,17 @@ public class AsyncMapSql {
 	
 	public static final void assertTable(SqlConnectionConfiguration sqlConf) {
 		Connection connection = SqlConnectionFactory.createConnection(sqlConf);
+		
+		
+		try {
+			CallableStatement statement = connection.prepareCall("CREATE TABLE IF NOT EXISTS TEST(ID VARCHAR(255) PRIMARY KEY, VALUE VARCHAR(255));");
+			
+			statement.execute();
+			
+			connection.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 }
