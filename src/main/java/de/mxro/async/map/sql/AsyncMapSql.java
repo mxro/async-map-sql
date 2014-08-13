7a2b5bd1927b67149758eae3de31cf7775998b22
mxro <mxro@nowhere.com>
2014-08-13 12:29:46 +1000

    Writing tests for SQL implementation of async map and fixing issues Autocommit Wed Aug 13 12:29:46 EST 2014

--- src/main/java/de/mxro/async/map/sql/AsyncMapSql.java
+++ src/main/java/de/mxro/async/map/sql/AsyncMapSql.java
@@ -10,33 +10,38 @@ import de.mxro.async.map.sql.internal.SqlConnectionFactory;
 
 public class AsyncMapSql {
 
-	public static final <V> AsyncMap<String, V> createMap(SqlAsyncMapConfiguration conf, SqlAsyncMapDependencies deps) {
+	public static final <V> AsyncMap<String, V> createMap(
+			SqlAsyncMapConfiguration conf, SqlAsyncMapDependencies deps) {
 		return new SqlAsyncMapImplementation<V>(conf, deps);
 	}
-	
-	public static final SqlAsyncMapConfiguration fromSqlConfiguration(final SqlConnectionConfiguration sqlConf) {
+
+	public static final SqlAsyncMapConfiguration fromSqlConfiguration(
+			final SqlConnectionConfiguration sqlConf) {
 		return new SqlAsyncMapConfiguration() {
-			
+
 			@Override
 			public SqlConnectionConfiguration sql() {
 				return sqlConf;
 			}
 		};
 	}
-	
+
 	public static final void assertTable(SqlConnectionConfiguration sqlConf) {
 		Connection connection = SqlConnectionFactory.createConnection(sqlConf);
-		
-		System.out.println("create table");
-		try {
-			CallableStatement statement = connection.prepareCall("CREATE TABLE IF NOT EXISTS "+sqlConf.getTableName()+"(ID VARCHAR(512) PRIMARY KEY, VALUE BLOB);");
-			
-			System.out.println(statement.execute());
 
-			//connection.close();
+		try {
+			CallableStatement statement = connection
+					.prepareCall("CREATE TABLE IF NOT EXISTS "
+							+ sqlConf.getTableName()
+							+ "(ID VARCHAR(512) PRIMARY KEY, VALUE BLOB);");
+
+			if (!sqlConf.getConnectionString().contains(":mem:")) {
+				connection.close();
+			}
+
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 }
