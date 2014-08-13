5ee687b0734d408b70c95c164a28528556a8f36e
mxro <mxro@nowhere.com>
2014-08-13 12:37:21 +1000

    Writing tests for SQL implementation of async map and fixing issues Autocommit Wed Aug 13 12:37:21 EST 2014

--- src/main/java/de/mxro/async/map/sql/AsyncMapSql.java
+++ src/main/java/de/mxro/async/map/sql/AsyncMapSql.java
@@ -20,6 +20,15 @@ import de.mxro.async.map.sql.internal.SqlConnectionFactory;
  */
 public class AsyncMapSql {
 
+	/**
+	 * Creates a new AsyncMap persisted by a JDBC compatible SQL database.
+	 * 
+	 * @param conf
+	 *            Configuration for this map.
+	 * @param deps
+	 *            Run-time dependencies for the maps.
+	 * @return
+	 */
 	public static final <V> AsyncMap<String, V> createMap(
 			SqlAsyncMapConfiguration conf, SqlAsyncMapDependencies deps) {
 		return new SqlAsyncMapImplementation<V>(conf, deps);
