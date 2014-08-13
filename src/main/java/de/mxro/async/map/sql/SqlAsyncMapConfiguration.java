be4c8b3db77ea7f7ec85e06f8acba77c18bde787
mxro <mxro@nowhere.com>
2014-08-13 12:38:21 +1000

    Writing tests for SQL implementation of async map and fixing issues Autocommit Wed Aug 13 12:38:21 EST 2014

--- src/main/java/de/mxro/async/map/sql/SqlAsyncMapConfiguration.java
+++ src/main/java/de/mxro/async/map/sql/SqlAsyncMapConfiguration.java
@@ -1,9 +1,21 @@
 package de.mxro.async.map.sql;
 
+import de.mxro.async.map.AsyncMap;
 import de.mxro.factories.Configuration;
 
+/**
+ * Configuration for a SQL linked {@link AsyncMap}.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
 public interface SqlAsyncMapConfiguration extends Configuration {
 
+	/**
+	 * SQL configuration for this map.
+	 * 
+	 * @return
+	 */
 	public SqlConnectionConfiguration sql();
-	
+
 }
