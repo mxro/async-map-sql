56871ed5fbc031ca00652eb0033393360c82091a
mxro <mxro@nowhere.com>
2014-08-13 10:40:46 +1000

    Developing SQL implementation of async map Autocommit Wed Aug 13 10:40:46 EST 2014

--- src/main/java/de/mxro/async/map/sql/SqlAsyncMapDependencies.java
+++ src/main/java/de/mxro/async/map/sql/SqlAsyncMapDependencies.java
@@ -4,6 +4,9 @@ package de.mxro.async.map.sql;
 
 
 import de.mxro.factories.Dependencies;
+import de.mxro.serialization.Serializer;
+import de.mxro.serialization.jre.StreamDestination;
+import de.mxro.serialization.jre.StreamSource;
 
 public interface SqlAsyncMapDependencies  extends Dependencies {
 
