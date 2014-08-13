5bbda29c2dfa8a5d7624eb7c1496d63800ecf8c6
mxro <mxro@nowhere.com>
2014-08-13 11:05:46 +1000

    Developing SQL implementation of async map Autocommit Wed Aug 13 11:05:46 EST 2014

--- src/main/java/de/mxro/async/map/sql/AsyncMapSql.java
+++ src/main/java/de/mxro/async/map/sql/AsyncMapSql.java
@@ -1,5 +1,12 @@
 package de.mxro.async.map.sql;
 
+import de.mxro.async.map.AsyncMap;
+import de.mxro.async.map.sql.internal.SqlAsyncMapImplementation;
+
 public class AsyncMapSql {
 
+	public static final <V> AsyncMap<String, V> createMap(SqlAsyncMapConfiguration conf, SqlAsyncMapDependencies deps) {
+		return new SqlAsyncMapImplementation<V>(conf, deps);
+	}
+	
 }
