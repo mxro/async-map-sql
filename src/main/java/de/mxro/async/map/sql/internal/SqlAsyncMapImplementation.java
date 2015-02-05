e4af221de27fc14f2ca86d64f453c152e50c4405
mxro <mxro@nowhere.com>
2015-02-05 14:43:54 +1100

    Fix bug that nodes with nearly identical names (but case) lead to data inconsistencies Autocommit Thu Feb 05 14:43:54 EST 2015

--- src/main/java/de/mxro/async/map/sql/internal/SqlAsyncMapImplementation.java
+++ src/main/java/de/mxro/async/map/sql/internal/SqlAsyncMapImplementation.java
@@ -298,10 +298,12 @@ public class SqlAsyncMapImplementation<V> implements AsyncMap<String, V> {
     @Override
     public V getSync(final String key) {
 
-        if (key.equals("mxrogm/mxrogm/xplr/.n/Manage/.n/Projects/.n/Appjangle_Apps/.n/Maybes/.n/Application_Deployment/Reading_It")) {
-            throw new RuntimeException();
-        }
-        System.out.println("get " + key);
+        // if
+        // (key.equals("mxrogm/mxrogm/xplr/.n/Manage/.n/Projects/.n/Appjangle_Apps/.n/Maybes/.n/Application_Deployment/Reading_It"))
+        // {
+        // throw new RuntimeException();
+        // }
+        // System.out.println("get " + key);
         return (V) getNode(key);
     }
 
