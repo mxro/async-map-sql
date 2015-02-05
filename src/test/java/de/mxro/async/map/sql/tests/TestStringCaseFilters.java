7094aa4ba9c9859bbdf2ac48820d6d0e5fb52313
mxro <mxro@nowhere.com>
2015-02-05 15:52:54 +1100

    Fix bug that nodes with nearly identical names (but case) lead to data inconsistencies Autocommit Thu Feb 05 15:52:54 EST 2015

--- src/test/java/de/mxro/async/map/sql/tests/TestStringCaseFilters.java
+++ src/test/java/de/mxro/async/map/sql/tests/TestStringCaseFilters.java
@@ -1,8 +1,20 @@
 package de.mxro.async.map.sql.tests;
 
+import org.junit.Test;
+
+import de.mxro.async.map.sql.internal.CaseInsensitiveToCaseSensitiveFilter;
+import de.mxro.async.map.sql.internal.CaseSensitiveToCaseInsensitiveFilter;
+
 public class TestStringCaseFilters {
 
+    @Test
     public void test() {
+
+        final CaseInsensitiveToCaseSensitiveFilter filterIn = new CaseInsensitiveToCaseSensitiveFilter();
+
+        final CaseSensitiveToCaseInsensitiveFilter filterOut = new CaseSensitiveToCaseInsensitiveFilter();
+
+        System.out.println(filterIn.apply("Something_with_Uppercase"));
 
     }
 
